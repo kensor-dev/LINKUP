a35f78d0b950cda8ebc33c718afbf5fb39af61d4
gigadude <valyaostape@gmail.com>
2026-04-15 15:41:27 +0500

    Добавлен файл socket.ts с функциями для управления подключением к сокету

---
+++ web/lib/socket.ts
@@ -0,0 +1,30 @@
+'use client'
+
+import { io, Socket } from 'socket.io-client'
+
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001'
+
+let socket: Socket | null = null
+
+export function getSocket(): Socket {
+  if (!socket) {
+    socket = io(SOCKET_URL, {
+      autoConnect: false,
+      transports: ['websocket'],
+    })
+  }
+  return socket
+}
+
+export function connectSocket(token: string) {
+  const s = getSocket()
+  s.auth = { token }
+  s.connect()
+  return s
+}
+
+export function disconnectSocket() {
+  if (socket?.connected) {
+    socket.disconnect()
+  }
+}
